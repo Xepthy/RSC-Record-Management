28f0a410fde5f95566a0f299c1b8f90d27a8ff9e
Xepthy <glitchlabyrith87@gmail.com>
2025-09-18 14:48:52 +0800

    Revert "AutoSave fix"
    This reverts commit c1d67f2c5421fbf929ffb9d29ea1d8b1a80232da.

--- pages/admin/inquiries/inquiry-manager.js
+++ pages/admin/inquiries/inquiry-manager.js
@@ -24,11 +24,9 @@ class InquiryManager {
     checkForChanges() {
         const currentRemarks = $('#remarksInput').val();
         const currentServices = this.getSelectedServices();
-        const currentStatus = $('#statusDropdown').val();
 
         // Check remarks changes
         const remarksChanged = currentRemarks !== this.originalValues.remarks;
-        const statusChanged = currentStatus !== this.originalValues.status;
 
         // Update remarks header
         const remarksHeader = $('.remarks-section .card-header h4');
@@ -38,24 +36,6 @@ class InquiryManager {
             }
         } else {
             remarksHeader.html('Remarks');
-        }
-
-        // Check status changes and update dropdown options
-        if (statusChanged) {
-            $('#statusDropdown option').each(function () {
-                const optionValue = $(this).val();
-                const optionText = $(this).text();
-
-                if (optionValue === currentStatus && !optionText.includes('*')) {
-                    $(this).text(optionText + ' *');
-                }
-            });
-        } else {
-            // Remove asterisks from all options
-            $('#statusDropdown option').each(function () {
-                const optionText = $(this).text().replace(' *', '');
-                $(this).text(optionText);
-            });
         }
 
         // Check individual service changes
@@ -389,14 +369,6 @@ class InquiryManager {
                     lastUpdated: serverTimestamp()
                 });
 
-                this.originalValues = {
-                    remarks: remarks,
-                    status: status,
-                    selectedServices: [...selectedServices]
-                };
-
-                this.checkForChanges();
-
                 this.clearSavedProgress();
                 console.log('Updates completed successfully');
                 this.showToast('Applied successfully!', 'success');
@@ -482,8 +454,7 @@ class InquiryManager {
 
         this.originalValues = {
             remarks: inquiry.remarks || '',
-            selectedServices: [...(inquiry.selectedServices || [])],
-            status: inquiry.status || ''
+            selectedServices: [...(inquiry.selectedServices || [])]
         };
 
         const clientName = inquiry.accountInfo ?
@@ -651,11 +622,7 @@ class InquiryManager {
             this.checkForChanges();
         });
 
-        $('#statusDropdown').on('change', () => {
-            this.autoSaveProgress();
-            this.checkForChanges();
-        });
-
+        $('#statusDropdown').on('change', () => this.autoSaveProgress());
         $('#applyRemarksBtn').on('click', () => this.handleRemarksApply());
     }
 
